819a602856a096b3777a9728026a11f4c1f507fd
Jose <j.a.ojeda@live.com>
2019-11-04 14:50:35 -0800

    created Main.js container

---
+++ src/containers/Main.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Header from '../components/Header';
+
+export default class Main extends Component {
+  static propTypes = {
+    title: PropTypes.string
+  }
+
+  render() {
+    return (
+      <>
+        <Header title={'Avatar: The Last Airbender'} />
+      </>
+    );
+  }
+}
