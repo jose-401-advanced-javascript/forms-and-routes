55eefaa6173f68577628bcdacde142b1cf5aa09d
Jose <j.a.ojeda@live.com>
2019-11-04 15:38:32 -0800

    added routes to Main.js

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Home from '../components/Home';
 
 export default class Main extends Component {
   static propTypes = {
@@ -10,10 +16,15 @@ export default class Main extends Component {
 
   render() {
     return (
-      <>
-        <Header title={'Avatar: The Last Airbender'} />
-        <Footer />
-      </>
+      <Router>
+        <>
+          <Header title={'Avatar: The Last Airbender'} />
+          <Switch>
+            <Route path="/home" component={Home} />
+          </Switch>
+          <Footer />
+        </>
+      </Router>
     );
   }
 }
