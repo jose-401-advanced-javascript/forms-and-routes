e1c5ffddaa7df70835b4055e393a7dc98747d4c9
Jose <j.a.ojeda@live.com>
2019-11-04 17:25:12 -0800

    imported Form.js to Home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Form from '../components/Form';
 
 export default class Home extends Component {
-  state = {
-    random: [],
-  }
-
-
   static propTypes = {
+    history: PropTypes.object.isRequired,
     random: PropTypes.arrayOf(PropTypes.shape({
       photoUrl: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired
     })).isRequired
+  }
+
+  state = {
+    random: [],
+    name: ''
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.history.push(`/about/${this.state.name}`);
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
   }
 
   render() {
@@ -19,7 +30,8 @@ export default class Home extends Component {
       <>
         <img src={this.random[0].photoUrl} alt={this.random[0].name} />
         <p>{this.random[0].name}</p>
-        <button></button>
+        <button>Get Random</button>
+        <Form name={this.state.name} handleSubmit={this.handleSubmit} handleChange={this.handleChange} />
       </>
     );
   }
