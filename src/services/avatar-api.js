fae5710d1767e0efbf1aedc258750cfb3d895401
Jose <j.a.ojeda@live.com>
2019-11-05 08:50:33 -0800

    added search fetch call

--- src/services/avatar-api.js
+++ src/services/avatar-api.js
@@ -2,3 +2,8 @@ export const getRandomCharacter = () => {
   return fetch('https://last-airbender-api.herokuapp.com/api/v1/characters/random')
     .then(results => results.json());
 };
+
+export const getSearchedCharacter = (name) => {
+  return fetch(`https://last-airbender-api.herokuapp.com/api/v1/characters?name=${name}`)
+    .then(results => results.json());
+}; 
