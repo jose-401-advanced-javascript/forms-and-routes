2e363eebb7c1f5e145cdb5dcadddc36c93c7d55c
Jose <j.a.ojeda@live.com>
2019-11-05 08:40:54 -0800

    created List.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Form from '../components/Form';
 import { getRandomCharacter } from '../services/avatar-api';
+
 export default class Home extends Component {
   state = {
     character: {},
---
+++ src/containers/List.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class List extends Component {
+
+  render() {
+
+    return (
+
+    );
+  }
+}
