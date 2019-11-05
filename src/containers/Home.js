14da895e398c2c712abb2d979f14dfd30ffc3245
Jose <j.a.ojeda@live.com>
2019-11-05 08:34:36 -0800

    updated Home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Form from '../components/Form';
-import { getRandom } from '../services/avatar-api';
-
+import { getRandomCharacter } from '../services/avatar-api';
 export default class Home extends Component {
-  static propTypes = {
-    history: PropTypes.object.isRequired,
-    random: PropTypes.arrayOf(PropTypes.shape({
-      photoUrl: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired
-    })).isRequired
-  }
-
   state = {
-    random: [],
+    character: {},
     name: '',
     string: ''
   }
 
+  static propTypes = {
+    history: PropTypes.object.isRequired,
+  }
+
   componentDidMount() {
-    getRandom()
-      .then(random => this.setState({ random }));
+    getRandomCharacter()
+      .then((fetchedChar) => {
+        this.setState({
+          character: fetchedChar[0]
+        });
+      });
   }
 
   handleSubmit = event => {
@@ -35,12 +34,11 @@ export default class Home extends Component {
   render() {
     return (
       <>
-        <img src={this.random[0].photoUrl} alt={this.random[0].name} />
-        <p>{this.random[0].name}</p>
+        <img src={this.state.character.photoUrl} alt={this.state.character.name} />
+        <p>{this.state.character.name}</p>
         <button>Get Random</button>
         <Form name={this.state.name} handleSubmit={this.handleSubmit} handleChange={this.handleChange} />
       </>
     );
-  }
+  }  
 }
-
